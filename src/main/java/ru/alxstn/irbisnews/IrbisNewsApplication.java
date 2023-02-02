7375910648c8f0ea203865f447aab4a509f196a5
AlxStenshin <alexander.stn@gmail.com>
2023-02-02 09:07:10 +0400

    Empty project init.

---
+++ src/main/java/ru/alxstn/irbisnews/IrbisNewsApplication.java
@@ -0,0 +1,13 @@
+package ru.alxstn.irbisnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrbisNewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IrbisNewsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/alxstn/irbisnews/IrbisNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alxstn.irbisnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrbisNewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
